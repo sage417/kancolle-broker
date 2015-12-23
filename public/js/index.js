9fe2827c2427f27d309089dfcd92b9d566958f4c
J.K.SAGE <sage417@foxmail.com>
2015-12-23 21:09:25 +0800

    update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,19 @@
-(function(){
-    function loadIframe(url){
-        if($("#redirect-r").prop('checked')){
+(function () {
+    function loadIframe(url) {
+        if ($("#redirect-r").prop('checked')) {
             window.location.replace(url);
-        }else{
+        } else {
             $("iframe").removeClass("hidden").attr("src", url);
             $("form").hide();
             $("footer").hide();
         }
     }
 
-    lsSupported = "localStorage" in window;
-    if(lsSupported){
+    var lsSupported = "localStorage" in window;
+    if (lsSupported) {
         $("#remember-box").removeClass("hidden");
-        cookie = window.localStorage.getItem('cookie');
-        if(cookie){
+        var cookie = window.localStorage.getItem('cookie');
+        if (cookie) {
             $("#remember").prop('checked', true);
             $.ajax({
                 type: "POST",
@@ -21,72 +21,73 @@
                 data: {
                     cookie: cookie
                 },
-                success:function (res) {
+                success: function (res) {
                     loadIframe(res.url);
                 },
-                error:function(type){
+                error: function (type) {
                     alert("Login Failed.");
                     console.log(type);
                     $("#remember").prop('checked', false);
                 },
-                beforeSend:function(){
+                beforeSend: function () {
                     $(':input').prop('disabled', true);
                 },
-                complete:function(){
+                complete: function () {
                     $(':input').prop('disabled', false);
                 }
             })
         }
-        $("#remember").on("change", function(e){
-            checked = $(e.target).prop('checked');
-            if(!checked){
+        $("#remember").on("change", function (e) {
+            var checked = $(e.target).prop('checked');
+            if (!checked) {
                 window.localStorage.clear();
             }
         });
 
-        redirect = window.localStorage.getItem('redirect');
-        if(redirect == 'redirect' || /Chrome/.test(window.navigator.userAgent)){
+        var redirect = window.localStorage.getItem('redirect');
+        if (redirect == 'redirect' || /Chrome/.test(window.navigator.userAgent)) {
             $("#redirect-r").prop('checked', true);
-        }else{
+        } else {
             $("#redirect-i").prop('checked', true);
         }
 
-        $("#redirect-i").on("change", function(){
-            window.localStorage.setItem('redirect','iframe');
+        $("#redirect-i").on("change", function () {
+            window.localStorage.setItem('redirect', 'iframe');
         })
-        $("#redirect-r").on("change", function(){
-            window.localStorage.setItem('redirect','redirect');
+        $("#redirect-r").on("change", function () {
+            window.localStorage.setItem('redirect', 'redirect');
         })
     }
+
     $("form").on("submit", function (e) {
         e.preventDefault();
 
-        uname = $("#login_id").val();
-        pass = $("#password").val();
-        data = $('form').serialize();
-        remember = $("#remember").prop('checked') && lsSupported;
+        //var uname = $("#login_id").val();
+        //var pass = $("#password").val();
+        var data = $('form').serialize();
+        var remember = $("#remember").prop('checked') && lsSupported;
         $.ajax({
             type: "POST",
             url: "login",
             data: data,
-            success:function (res,status) {
-                if(status === 200){
-                    if(remember){
-                        window.localStorage.setItem('cookie', res.cookie);
-                    }
-                    loadIframe(res.url);
+            success: function (res, status) {
+                console.log(status);
+                if (remember) {
+                    window.localStorage.setItem('cookie', res.cookie);
                 }
+                loadIframe(res.url);
             },
-            error:function(data,status,error){
+            error: function (data, status, error) {
+                console.log(status);
                 console.log(error);
                 alert("Login Failed.");
                 location.reload();
                 //$("#remember").prop('checked', false);
             },
-            beforeSend:function(){
+            beforeSend: function () {
                 $(':input').prop('disabled', true);
             },
-            complete:function(){
+            complete: function () {
                 $(':input').prop('disabled', false);
             }
         })
